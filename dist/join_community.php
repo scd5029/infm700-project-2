5086b9787ed51b7e5c26ffe1cb637303e6fa6166
Daiwei Lu <daiweilu123@gmail.com>
2013-10-21 02:02:01 -0400

    Responsive map block

--- dist/join_community.php
+++ dist/join_community.php
@@ -127,55 +127,6 @@
 
 </div><!-- END .se-content -->
 
-<!-- Map Block -->
-<div class="se-map-block">
-  <div class="se-map-block-canvas" id="se-map-block-canvas"></div>
-  <div class="se-map-block-detail">
-    <div class="se-map-block-d-container">
-      <div class="se-map-block-d-inner">
-        <header class="se-map-block-d-i-header">Find us at:</header>
-        <address class="se-map-block-d-i-address">
-          27 South Patterson Park Avenue<br/>
-          Baltimore, MD 21231
-        </address>
-        <a href="" class="se-map-block-d-i-get-direction">Get Directions from Google</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Footer -->
-<footer class="se-footer">
-  <div class="se-footer-inner">
-    <!-- Left -->
-    <div class="se-footer-i-contact-info">
-      <header class="se-footer-section-header">Contact us</header>
-      <ul class="se-footer-section-list">
-        <li><span>Phone: </span><span>410-276-3676</span></li>
-        <li><span>Fax: </span><span>410-327-7592</span></li>
-        <li><span>Email: </span><span>Friends@PattersonPark.com</span></li>
-      </ul>
-    </div>
-
-    <!-- Middle -->
-    <div class="se-footer-i-subscribe">
-      <header class="se-footer-section-header">Subscribe Email Newsletter</header>
-      <input type="email" class="se-footer-section-input" placeholder="Subscribe to Email Newsletter">
-      <button class="btn btn-success">Subscribe</button>
-    </div>
-
-    <!-- Right -->
-    <div class="se-footer-i-search">
-      <header class="se-footer-section-header">Search this Website</header>
-      <input type="text" class="se-footer-section-input" placeholder="">
-      <button class="btn btn-primary">Search</button>
-    </div>
-
-  </div>
-</footer>
-
-<div class="se-footer-copyright">Copyright © 2013 Friends of Patterson Park. All Rights Reserved.</div>
-
 
 <?php require "partials/_foot.php" ?>
 <?php require "partials/_scripts.php" ?>
