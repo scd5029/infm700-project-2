f92ec6ee391861d98e61dc6f8a327819c2f30d97
Daiwei Lu <daiweilu123@gmail.com>
2013-10-24 00:40:31 -0400

    Add active state for local-nav

--- dist/_partials/_local_nav.php
+++ dist/_partials/_local_nav.php
@@ -1,6 +1,7 @@
 <?php
-preg_match("/^\/(\w+)(?:\/\w+\.php)$/", $_SERVER['REQUEST_URI'], $matches);
+preg_match("/^\/(\w+)\/(\w+\.php)$/", $_SERVER['REQUEST_URI'], $matches);
 $current_folder = $matches[1];
+$current_file   = $matches[2];
 
 foreach ($navbar_structure as $title => $sub_items) {
   if ($current_folder == to_underscore($title)) {
@@ -14,8 +15,11 @@ foreach ($navbar_structure as $title => $sub_items) {
 <div class="md-local-nav">
   <ul class="nav">
     <?php for ($i=0; $i < count($local_nav_titles); $i++) { ?>
-    <li class="">
-      <a href="<?= to_dir($current_folder).to_file($local_nav_titles[$i]) ?>"><?=$local_nav_titles[$i]?></a>
+    <li>
+      <a class="<?php if ($current_file == to_file($local_nav_titles[$i])) echo 'active'; ?>"
+         href="<?= to_dir($current_folder).to_file($local_nav_titles[$i]) ?>">
+        <?=$local_nav_titles[$i]?>
+      </a>
     </li>
     <?php } ?>
   </ul>
